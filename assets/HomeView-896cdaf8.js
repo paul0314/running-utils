12f337a002c015ab9ce39899f5cef80674746f21
pvoelkel <Paul.Voelkel@adesso.de>
2024-06-16 12:36:16 +0200

    adding dist

---
+++ assets/HomeView-896cdaf8.js
@@ -0,0 +1,1 @@
+import{d as e,_ as o,o as n,c as t}from"./index-15dd1843.js";const _=e({name:"HomeView",components:{}});function s(c,a,p,r,d,l){return n(),t("div",null,"Hello world")}const m=o(_,[["render",s],["__scopeId","data-v-e8152020"]]);export{m as default};
